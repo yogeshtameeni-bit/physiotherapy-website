4ea831eea5432f9b1afa83e8aaa24763031e5e8e
Yogesh <yogesh.tameeni@gmail.com>
2026-07-18 11:56:01 +0530

    Point http://www.niyatphysio-api.shop/api

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,9 @@ import axios from "axios";
 
 const api = axios.create({
 
-  baseURL: "http://localhost:5151/api"
+  // baseURL: "http://localhost:5151/api"
+  //baseURL: "http://localhost:8085/api" // IIS
+  baseURL: "http://www.niyatphysio-api.shop/api"
 
 });
 
